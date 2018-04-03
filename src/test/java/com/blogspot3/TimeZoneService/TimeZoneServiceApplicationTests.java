953e7d8d6e46ac0768bcc905da1b340a6bec89a7
jakub.jozwicki <jakub.jozwicki@allegro.pl>
2018-04-03 16:53:45 +0200

    added simple TZ test

--- src/test/java/com/blogspot3/TimeZoneService/TimeZoneServiceApplicationTests.java
+++ src/test/java/com/blogspot3/TimeZoneService/TimeZoneServiceApplicationTests.java
@@ -5,12 +5,21 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TimeZoneServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testOffsetSydney() {
+		TimeZoneServiceApplication app = new TimeZoneServiceApplication();
+		String expectedPlus10 = app.getTimeZoneOffset("2018-04-01T18:00:00", "Australia/Sydney");
+		assertThat("+10:00").isEqualTo(expectedPlus10);
 	}
 
 }
